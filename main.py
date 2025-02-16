5608fd2965ca827812db37f3c3ffd4e655a68110
Nicolae M. <nicolaemargamd@gmail.com>
2025-02-16 21:07:44 +0200

    Defined Grammar class

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from graphviz import Digraph, Source
 import random 
-
+class Grammar:
+    def __init__(self, non_terminals, terminals, productions, start_symbol):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
+        self.productions = productions
+        self.start_symbol = start_symbol
+        
 
 if __name__ == "__main__":
+    grammar = Grammar(
+        non_terminals={"S", "B", "C", "D"},
+        terminals={"a", "b", "c"},
+        productions={
+            "S": ["aB"],
+            "B": ["bS", "aC", "b"],
+            "C": ["bD"],
+            "D": ["a", "bC", "cS"]
+        },
+        start_symbol="S"
+    )
     pass
