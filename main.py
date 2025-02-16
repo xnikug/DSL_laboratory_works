249e9439d1be797f3352f5d2b9864d37cd2cab21
Nicolae M. <nicolaemargamd@gmail.com>
2025-02-16 23:12:29 +0200

    Generation of words defined by grammar

--- main.py
+++ main.py
@@ -2,13 +2,49 @@ import random
 
 # For type 3 grammars in the Chomsky hierarchy
 class Grammar:
+    # Constructor for grammar
     def __init__(self, non_terminals, terminals, productions, start_symbol):
         self.non_terminals = non_terminals
         self.terminals = terminals
         self.productions = productions
         self.start_symbol = start_symbol
         self.type = self.get_type()
+        self.words = []
     
+    # Generate all possible strings up to a certain depth
+    def generate_string(self, symbol=None, len=0, max_len=100):
+        # If no symbol is provided, use the start symbol
+        if symbol is None:
+            symbol = self.start_symbol
+
+        # It may return the non-terminal symbol to limit the recursion depth
+        if len > max_len:
+            return symbol
+
+        # Symbol is terminal
+        if symbol in self.terminals:
+            return symbol
+
+        if symbol in self.productions:
+            # Random production
+            production = random.choice(self.productions[symbol])
+            result = ''
+            for sym in production:
+                result += self.generate_string(sym, len + 1, max_len)
+            return result
+
+        # The symbol doesn't match anything in the production
+        return ''
+    # Return a certain number of valid words that is contained in the language 
+    def generate_strings(self, num):
+        self.words = []
+        while len(self.words) < num:
+            word = self.generate_string()
+            # Only valid and unique words append 
+            if all(word[i] in self.terminals for i in range(len(word))) and word not in self.words:
+                self.words.append(word)
+        return self.words
+    # Checks it's type of grammar based on the productions rules
     def get_type(self):
         left_type = False
         right_type = False
@@ -16,18 +52,17 @@ class Grammar:
         total_rules = []
         for productions_rule in self.productions.values():
             total_rules += productions_rule
-        print(total_rules)
 
         # Get the end result of grammar rules which have length of 2
-        rules = [rule for rule in total_rules if len(rule) == 2]
-        
-        for production in rules:
-        
+        rules = [rule for rule in total_rules if len(rule) >= 2]
+        print(rules)
 
-            if production[0] in self.terminals and production[1] in self.non_terminals:
-                left_type = True
-            elif production[0] in self.non_terminals and production[1] in self.terminals:
+        for production in rules:
+
+            if all(production[i] in self.terminals for i in range(len(production) - 1)) and production[-1] in self.non_terminals:
                 right_type = True
+            elif production[0] in self.non_terminals and  all(production[i] in self.terminals for i in range(1, len(production))):
+                left_type = True
             else:
                 raise ValueError('Invalid type 3 grammar definition')
 
@@ -46,9 +81,10 @@ if __name__ == "__main__":
         productions={
             "S": ["aB"],
             "B": ["bS", "aC", "b"],
-            "C": ["bD"],    
+            "C": ["bD"],
             "D": ["a", "bC", "cS"]
         },
         start_symbol="S"
     )
-    pass
+    print(grammar.get_type())
+    print(grammar.generate_strings(100))
